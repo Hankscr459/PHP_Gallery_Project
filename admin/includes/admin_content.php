6f1b33d6dc94858fab5dd0e39cc34797ee1d715f
Hankscr459 <defcw58@gmail.com>
2021-03-03 16:20:03 +0800

    Static Method Usage and Challenge

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -10,13 +10,17 @@
             </h1>
             <?php
 
-                $result_set = User::find_all_users();
+                // $result_set = User::find_all_users();
 
-                while($row = mysqli_fetch_array($result_set)) {
+                // while($row = mysqli_fetch_array($result_set)) {
 
-                    echo $row['username'] . "<br>";
-                }
+                //     echo $row['username'] . "<br>";
+                // }
+
+                $found_user = User::find_user_by_id(2);
+                echo $found_user['username'];
             ?>
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
