d598d08ba0e9396827958323dd6e19c5b144cedd
Hankscr459 <defcw58@gmail.com>
2021-03-03 18:53:14 +0800

    Adding our Instantiation Method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -23,12 +23,17 @@
         public static function find_this_query($sql) {
             global $database;
             $result_set = $database->query($sql);
+            $the_object_array = array();
 
-            return $result_set;
+            while($row = mysqli_fetch_array($result_set)) {
+                $the_object_array[] = self::instantation($row);
+            }
+
+            return $the_object_array;
         }
 
-        public static function intantation($the_record) {
-            $the_object = new self;
+        public static function instantation($the_record) {
+            $the_object = new self();
                 
             // $the_object->id         = $found_user['id'];
             // $the_object->username   = $found_user['username'];
@@ -38,7 +43,7 @@
 
             foreach ($the_record as $the_attribute => $value) {
                 if ($the_object->has_the_attribute($the_attribute)) {
-                    $the_object->the_attribute = $value;
+                    $the_object-> $the_attribute = $value;
                 }
             }
 
@@ -49,5 +54,6 @@
             $object_properties = get_object_vars($this);
             return array_key_exists($the_attribute, $object_properties);
         }
+        
     }
 ?>
