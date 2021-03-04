d34801b3e8d11674c0d066bc1eb6a807ab8a4777
Hankscr459 <defcw58@gmail.com>
2021-03-04 21:48:30 +0800

    Modifying the properties method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,6 +2,7 @@
     class User {
 
         protected static $db_table = "users";
+        protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name');
         public $id;
         public $username;
         public $password;
@@ -74,12 +75,9 @@
 
         public function create() {
             global $database;
-            $sql = "INSERT INTO " . self::$db_table . " (username, password, first_name, last_name)";
-            $sql .= "VALUES ('";
-            $sql .= $database->escape_string($this->username) . "', '";
-            $sql .= $database->escape_string($this->password) . "', '";
-            $sql .= $database->escape_string($this->first_name) . "', '";
-            $sql .= $database->escape_string($this->last_name) . "')";
+            $properties = $this->properties();
+            $sql = "INSERT INTO " . self::$db_table . "(" . implode(",", array_keys($properties)) .")";
+            $sql .= "VALUES ('". implode("','", array_values($properties)) ."')";
 
             if ($database->query($sql)) {
                 $this->id = $database->the_insert_id();
@@ -91,6 +89,7 @@
 
         public function update() {
             global $database;
+            
             $sql = "UPDATE " . self::$db_table . " SET ";
             $sql .= "username= '" . $database->escape_string($this->username)     . "', ";
             $sql .= "password= '" . $database->escape_string($this->password)     . "', ";
@@ -101,6 +100,18 @@
             $database->query($sql);
 
             return (mysqli_affected_rows($database->connection) == 1) ? true : false;
+        }
+
+        protected function properties() {
+            // return get_object_vars($this);
+
+            $properties = array();
+            foreach (self::$db_table_fields as $db_field) {
+                if (property_exists($this, $db_field)) {
+                    $properties[$db_field] = $this->db_field;
+                }
+            }
+            return $properties;
         }
 
         public function save() {
