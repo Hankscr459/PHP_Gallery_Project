753688d68a2ec397c2d307d2f79112a492ae3622
Hankscr459 <defcw58@gmail.com>
2021-03-04 14:22:54 +0800

    fixed login

--- admin/login.php
+++ admin/login.php
@@ -3,11 +3,11 @@
 <?php
     if($session->is_signed_in()) {
         redirect("index.php");
-
-        if(isset($_POST['submit'])) {
-            $username = trim($_POST['username']);
-            $password = trim($_POST['password']);
-        }
+    } 
+    
+    if(isset($_POST['submit'])) {
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
 
         // Method to check database user
 
