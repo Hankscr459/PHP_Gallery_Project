7b6d3a4c3d242348e970857d212d4bcef53d83b7
Hankscr459 <defcw58@gmail.com>
2021-03-05 13:38:02 +0800

    Adding Class Properties

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,5 +1,13 @@
 <?php
     class Photo extends DB_object {
-        //
+
+        protected $db_table = "photos";
+        protected $db_table_fields = array('Photo_id', 'title', 'description', 'filename', 'type', 'size');
+        public $Photo_id;
+        public $title;
+        public $description;
+        public $filename;
+        public $type;
+        public $size;
     }
 ?>
