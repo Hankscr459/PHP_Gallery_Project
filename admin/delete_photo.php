6be7ede469e2e79a3d7872575278045e24affe21
Hankscr459 <defcw58@gmail.com>
2021-03-05 17:33:40 +0800

    DELETE Page Part # 1

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -1,6 +1,17 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/init.php"); ?>
 <?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
 
 <?php
-    echo "it works";
+    if (empty($_GET['photo_id'])) {
+        redirect("photos.php");
+    }
+
+    $photo = Photo::find_by_id($_GET['photo_id']);
+
+    if ($photo) {
+        $photo->delete_photo();
+    } else {
+        redirect("photos.php");
+    }
+
 ?>
