2e730d57cf3190c98205b1f46b1de6e539f0b4fa
Hankscr459 <defcw58@gmail.com>
2021-03-06 16:34:04 +0800

    Self Instantiation Comment Method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -7,6 +7,21 @@
         public $photo_id;
         public $author;
         public $body;
+
+        public static function create_comment($photo_id, $author="Hank", $body="") {
+            if (!empty($photo_id) && !empty($author) && !empty($body)) {
+
+                $comment = new Comment();
+
+                $comment->photo_id = (int)$photo_id;
+                $comment->author = $author;
+                $comment->body = $body;
+
+                return $comment;
+            } else {
+                return false;
+            }
+        }
         
     }
 ?>
