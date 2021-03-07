fc107e5762f261ca36de75e3f178d561d8d2757e
Hankscr459 <defcw58@gmail.com>
2021-03-07 11:35:37 +0800

    Dashboard HTML Snippets Inclusion

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,68 +8,107 @@
                 <small>Subheading</small>
 
             </h1>
-            <?php
-                // $user = new User();
+            
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">4</div>
+                                    <div>New Views</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                    <div>Page View from Gallery</div>
+                                <span class="pull-left">View Details</span> 
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> 
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-                // $user->username = "pikachu";
-                // $user->password = "123456";
-                // $user->first_name = "muscular";
-                // $user->last_name = "pikachu";
-
-                // $user->create();
-
-
-                // $users = User::find_all();
-
-                // foreach ($users as $user) {
-                //     echo $user->username;
-                // }
-
-
-
-
-                // $user = User::find_user_by_id(3);
-
-                // $user->last_name = "Pikchu";
-
-                // $user->update();
-
-                // $user = User::find_by_id(1);
-                // echo $user->username;
-
-                $photo = Photo::find_by_id(3);
-                echo $photo->filename;
-
-                // $user = new User();
-                // $user->username = "Edwin";
-
-                // $user->save();
+                    <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-photo fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">7</div>
+                                    <div>Photos</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Photos in Gallery</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
 
-                // $photos = Photo::find_all();
-                // foreach ($photos as $photo) {
-                //     echo $photo->title;
-                // }
+                    <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">8
 
-                // $photo = new Photo();
+                                    </div>
 
-                // $photo->title = "just some test title";
-                // $photo->size = 20;
+                                    <div>Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Users</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-                // $photo->create();
+                    <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-support fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">8</div>
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Comments</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-                // echo INCLUDES_PATH;
 
-            ?>
-           
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
+            </div> <!--First Row-->
         </div>
     </div>
     <!-- /.row -->
