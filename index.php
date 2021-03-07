edaa6b445b11e4fb63fd0242528700ade8262a32
Hankscr459 <defcw58@gmail.com>
2021-03-07 15:21:39 +0800

    Setting Up our Pagination Variables

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include("includes/header.php"); ?>
 <?php 
+
+    $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+
+    $item_per_page = 4;
+    $items_total_count = Photo::count_all();
+
     $photos = Photo::find_all();
 ?>
 
